c53af50c487489ba8c5f21f8e1da1a8e9f06cdb3
4jemimansanda <gemlovesgems44@gmail.com>
2026-03-30 21:51:08 +0100

    Fix on redirect link

--- public/js/lesson.js
+++ public/js/lesson.js
@@ -273,7 +273,7 @@ function renderCurrentItem(itemKey) {
                     body: JSON.stringify({ courseId, userId })
                 });
 
-                window.location.href = `/progress.html?courseId=${courseId}`;
+                window.location.href = `public/progress.html`;
             } catch (error) {
                 console.error(error);
                 alert('Error saving completion.');
